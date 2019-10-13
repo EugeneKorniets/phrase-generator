c95f3371b16b168449bbf20e23df1de707e41816
Eugene Korniets <evgeniy.korniets@gmail.com>
2019-10-14 00:49:30 +0300

    fix code style

--- src/pages/main-page.vue
+++ src/pages/main-page.vue
@@ -35,7 +35,11 @@
                   viewbox="0 0 48 48"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M 18 32.34 l -8.34 -8.34 -2.83 2.83 11.17 11.17 24 -24 -2.83 -2.83 z" stroke="#3da35a" fill="transparent"/>
+                  <path
+                    d="M 18 32.34 l -8.34 -8.34 -2.83 2.83 11.17 11.17 24 -24 -2.83 -2.83 z"
+                    stroke="#3da35a"
+                    fill="transparent"
+                  />
                 </svg>
               </span>
             </label>
